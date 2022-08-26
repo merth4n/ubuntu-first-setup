90783cb30be7e4f98907f00b7d59d081c86972d0
Merthan Karaman <79408454+merthankaraman@users.noreply.github.com>
2022-08-26 21:10:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 echo "$(whoami) ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 echo "alias full-update='sudo apt update -y && sudo apt upgrade -y'" >> ~/.bashrc
 full-update
+sudo apt install glances -y
